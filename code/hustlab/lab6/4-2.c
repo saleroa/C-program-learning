5688e731fb269f44c48594b3fdd4c7ed5ea43de1
saleroa <2985299550@qq.com>
2024-09-19 13:48:16 +0800

    fix

--- code/hustlab/lab6/4-2.c
+++ code/hustlab/lab6/4-2.c
@@ -1,43 +1,14 @@
-// #include <stdio.h>
-
-// // void turn(char **a, int n, int m)
-// // {
-// //     for (int i = m-1; i > -1; i--)
-// //     {
-// //         for (int j = 0; j < n; j++)
-// //         {
-// //             printf("%c",*(*(a+j)+i));
-// //         }
-// //         printf("\n");
-// //     }
-// // }
-
-// int main()
-// {
-//     int n, m;
-//     printf("please input n,m: \n");
-//     scanf("%d %d", &n, &m);
-//     getchar();
-//     getchar();
-//     // 变长数组相关
-//     char a[n][m];
-//     for (int i = 0; i < n; i++)
-//     {
-//         for (int j = 0; j < m; j++)
-//         {
-//             scanf("%c", &a[i][j]);
-//         }
-//     }
-
-//     // turn(a, n, m);
-
-//     return 0;
-// }
-
-
 #include<stdio.h>
-void turn(int *);
+
+
 int m, n;
+
+void turn(int *);
+void turn_1(int a [][m]) ;
+void turn_2(int a [][m]) ;
+
+
+
 int main(void) {
     scanf("%d %d", &n, &m);
     int matrix[n][m];
@@ -47,15 +18,48 @@ int main(void) {
         }
     }
     printf("\nInput complete!\n");
-    int *ptr=(int*)&matrix[0];
+
+    // int *ptr=(int*)&matrix[0]; // 等价下面操作，为了获取到第一个 int 的地址，方便做加法
+    int *ptr=&matrix[0][0];
+
     turn(ptr);
+    turn_1(matrix);
+    turn_2(matrix);
     return 0;
 }
+
+
+// 数组就是一块连续内存,只有字节与字节的分隔，即使是二维数组，也是一块连续内存，每个数组之间没有明确界限
+// 所以传入二维数组的时候需要 a[][m] , 传入一个 m 来对二维数组进行数组间的间隔，方便指针运算，而传入 char** 是指针的指针，没有间隔
+
+// 未知情况下的
 void turn(int *ptr) {
     for (int i = m-1; i >= 0; i--) {
         for (int j = 0; j < n; j++) {
             // 希望打印matrix[j][i]
             printf("%d ", *(ptr + j*m + i));
+        }
+        printf("\n");
+    }   
+}
+
+
+// 预定输入 2、3
+void turn_1(int a [][m]) {
+    for (int i = m-1; i >= 0; i--) {
+        for (int j = 0; j < n; j++) {
+            // 希望打印matrix[j][i]
+            printf("%d ", a[j][i]);
+        }
+        printf("\n");
+    }   
+}
+// 预定输入 2、3
+void turn_2(int a [][m]) {
+    for (int i = m-1; i >= 0; i--) {
+        for (int j = 0; j < n; j++) {
+            // 希望打印matrix[j][i]
+            printf("%d ", *(*(a+j)+i));
         }
         printf("\n");
     }   
